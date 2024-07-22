e83ae337cc346178613b29ec411eca2be31e1b41
startang141 <bintangadiyatma141@gmail.com>
2024-07-22 15:52:24 +0700

    Using local storage on form login

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -4,8 +4,8 @@ import InputForm from "../Elements/InputForm";
 const FormLogin = () => {
   const handleLogin = (event) => {
     event.preventDefault();
-    console.log(event.target.email.value);
-    console.log(event.target.password.value);
+    localStorage.setItem("email", event.target.email.value);
+    localStorage.setItem("password", event.target.password.value);
     window.location.href = "/products";
   };
   return (
